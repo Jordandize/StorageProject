a288eadf0f06b375cc5831d78a9e0501518add36
Constantine <675kostya@gmail.com>
2018-10-22 21:46:53 +0300

    File structure added

---
+++ src/main/java/ua/edu/ukma/gpd/storage/StorageProjectApplication.java
@@ -0,0 +1,12 @@
+package ua.edu.ukma.gpd.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageProjectApplication.class, args);
+	}
+}
