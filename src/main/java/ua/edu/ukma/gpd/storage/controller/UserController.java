0c78a8b9730f595b7dea29099aaedd82f7825c9c
Constantine <675kostya@gmail.com>
2018-10-25 18:58:41 +0300

    Connected with DB

--- src/main/java/ua/edu/ukma/gpd/storage/controller/UserController.java
+++ src/main/java/ua/edu/ukma/gpd/storage/controller/UserController.java
@@ -1,20 +1,45 @@
 package ua.edu.ukma.gpd.storage.controller;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ua.edu.ukma.gpd.storage.service.UserService;
 
-@RestController("/user")
+@RestController
+@RequestMapping("/users")
 public class UserController {
 	
+	@SuppressWarnings("unused")
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private DataSource dataSource;
 
-	@GetMapping("/")
+	@GetMapping
 	public String users() {
-		return userService.getAll().toString();
+		//Test If DB is available
+		JdbcTemplate template = new JdbcTemplate(dataSource);
+		Integer integer = template.query("SELECT 1", new ResultSetExtractor<Integer>() {
+
+			@Override
+			public Integer extractData(ResultSet rs) throws SQLException, DataAccessException {
+				rs.next();
+				return rs.getInt(1);
+			}
+			
+		});
+		return integer.toString();
 	}
 
 }
--- src/main/java/ua/edu/ukma/gpd/storage/service/impl/UserServiceImpl.java
+++ src/main/java/ua/edu/ukma/gpd/storage/service/impl/UserServiceImpl.java
@@ -1,5 +1,49 @@
 package ua.edu.ukma.gpd.storage.service.impl;
 
-public class UserServiceImpl {
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import ua.edu.ukma.gpd.storage.entity.User;
+import ua.edu.ukma.gpd.storage.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public List<User> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User add(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User edit(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User delete(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User getById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User getByEmail(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
