3bf3281c20b66aaccdcdcb38ef8d4825189260a6
stas <stas.stepaniuk@gmai.com>
2018-11-05 13:40:18 +0200

    sth

--- Backend/src/main/java/ua/edu/ukma/gpd/storage/configuration/auth/RestAuthenticationSuccessHandler.java
+++ Backend/src/main/java/ua/edu/ukma/gpd/storage/configuration/auth/RestAuthenticationSuccessHandler.java
@@ -7,12 +7,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 import org.springframework.security.web.savedrequest.RequestCache;
 import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @Component
 public class RestAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
@@ -22,6 +27,14 @@ public class RestAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuc
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, 
     		Authentication authentication) throws ServletException, IOException {
+    	ObjectMapper mapper = new ObjectMapper();
+    	ObjectNode responseUser = mapper.createObjectNode();
+    	responseUser.put("email", authentication.getName());
+    	ArrayNode roles = mapper.createArrayNode();
+    	for(GrantedAuthority auth: authentication.getAuthorities())
+    		roles.add(auth.toString());
+    	responseUser.putPOJO("roles", roles);
+    	response.getWriter().write(responseUser.toString());
         SavedRequest savedRequest = requestCache.getRequest(request, response);
         if(savedRequest == null) {
             clearAuthenticationAttributes(request);
