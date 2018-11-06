8134d6f81bcad2884d8f285df2615a8471be4e6a
stas <stas.stepaniuk@gmai.com>
2018-11-06 17:33:29 +0200

    order++

--- Frontend/src/app/order/order.component.ts
+++ Frontend/src/app/order/order.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from "@angular/core";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {ActivatedRoute, Router} from "@angular/router";
+import {validate} from "codelyzer/walkerFactory/walkerFn";
 
 @Component({templateUrl: 'order.component.html'})
 export class OrderComponent implements OnInit {
@@ -9,6 +10,9 @@ export class OrderComponent implements OnInit {
   loading = false;
   submitted = false;
   returnUrl: string;
+
+
+
 
   constructor(
     private http: HttpClient,
@@ -19,7 +23,79 @@ export class OrderComponent implements OnInit {
     // private alertService: AlertService
   ) {}
   ngOnInit() {
+    this.orderForm = this.formBuilder.group({
+      ordStatus: ['', Validators.required],
+      ordType: ['', Validators.required],
+      parentId: ['', Validators.required],
+      createdDate: ['', Validators.required],
+      modifiedDate: ['', Validators.required],
+      crBy: ['', Validators.required],
+      assTo: ['', Validators.required],
+      comment: [''],
+      isArchived: ['', Validators.required],
+
+      //Array here in the future
+          category:['', Validators.required],
+          product:['', Validators.required],
+          qnt:['', Validators.required],
+      //
+    });
+    const head = new HttpHeaders({'Content-Type': 'application/json'});
+    return this.http.get('http://localhost:4200/order', {headers: head}).subscribe(
+      data => {},
+      error => {
+        this.loading = false;
+      }
+    );
+    //
+    //HERE receive JSON object asd fill "category" and "product" fields
+    //
   }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.orderForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.orderForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    const head = new HttpHeaders({'Content-Type': 'application/json'});
+
+    const order = {
+      'ordStatus': this.f.ordStatus.value,
+      'ordType': this.f.ordType.value,
+      'parentId': this.f.parentId.value,
+      'createdDate': this.f.createdDate.value,
+      'modifiedDate': this.f.modifiedDate.value,
+      'crBy': this.f.crBy.value,
+      'assTo': this.f.assTo.value,
+      'comment': this.f.comment.value,
+      'isArchived': this.f.isArchived.value,
+      'category': this.f.category.value,
+      'product': this.f.product.value,
+      'qnt': this.f.qnt.value,
+      'phone': this.f.phone.value
+    };
+    return this.http.post('http://localhost:4200/order', order, {headers: head}).subscribe(
+      data => {
+        this.router.navigate(['/home']);
+      },
+      error => {
+        this.loading = false;
+      }
+    );
+  }
+
 }
+
+
+
+
+
 
 
