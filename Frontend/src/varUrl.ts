432bb6bf7233f03e49592380f6288898138d71f2
Jordandize <675kostya@gmail.com>
2018-11-26 22:05:04 +0200

    Update varUrl.ts

--- Frontend/src/varUrl.ts
+++ Frontend/src/varUrl.ts
@@ -1,3 +1,3 @@
-export const baseUrl = 'http://localhost:8080'  ;
-//export const baseUrl = 'https://storage-pro.herokuapp.com'  ;
+//export const baseUrl = 'http://localhost:8080'  ;
+export const baseUrl = 'https://storage-pro.herokuapp.com'  ;
 
