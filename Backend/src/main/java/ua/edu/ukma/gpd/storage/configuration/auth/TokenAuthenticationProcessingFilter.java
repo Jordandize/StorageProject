e4b54ec4612946e033a2e2254c047698c14b7524
Constantine <675kostya@gmail.com>
2018-11-29 19:29:52 +0200

    Security conf

--- Backend/src/main/java/ua/edu/ukma/gpd/storage/configuration/auth/TokenAuthenticationProcessingFilter.java
+++ Backend/src/main/java/ua/edu/ukma/gpd/storage/configuration/auth/TokenAuthenticationProcessingFilter.java
@@ -7,6 +7,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -25,19 +27,26 @@ public class TokenAuthenticationProcessingFilter extends AbstractAuthenticationP
 
 	public TokenAuthenticationProcessingFilter(String defaultFilterProcessesUrl) {
 		super(defaultFilterProcessesUrl);
-		setAuthenticationSuccessHandler((request, response, authentication) ->
-			SecurityContextHolder.getContext().setAuthentication(authentication)
+		setAuthenticationSuccessHandler((request, response, authentication) -> {
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+
+			System.out.println("sscc2");
+		}
 		);
 	}
 
 	@Override
 	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
-			throws AuthenticationException, IOException, ServletException {
+			throws AuthenticationException {
 		String token = request.getHeader(SecurityConfiguration.HEADER_SECURITY_TOKEN);
 		Authentication userAuth = tokenToAuth(token);
 		if(userAuth == null) {
-			throw new AuthenticationServiceException(
-					"TokenAuthenticationProcessingFilter: Authentication for token [" + token + "] failed");
+			//throw new AuthenticationServiceException(
+				//	"TokenAuthenticationProcessingFilter: Authentication for token [" + token + "] failed");
+			System.out.println("Anonimous?2");
+			throw new AuthenticationCredentialsNotFoundException("hz");
+		} else {
+			System.out.println("Not anonimous!");
 		}
 		return userAuth;
 	}
@@ -45,6 +54,7 @@ public class TokenAuthenticationProcessingFilter extends AbstractAuthenticationP
 	@Override
 	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
 			Authentication authentication) throws IOException, ServletException {
+		System.out.println("sscc");
 		super.successfulAuthentication(request, response, chain, authentication);
 		chain.doFilter(request, response);
 	}
@@ -52,25 +62,33 @@ public class TokenAuthenticationProcessingFilter extends AbstractAuthenticationP
 	@Override
 	protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException failed) throws IOException, ServletException {
-		//super.unsuccessfulAuthentication(request, response, failed);
-		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+		super.unsuccessfulAuthentication(request, response, failed);
+		System.out.println("unss");
+		//response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
 	}
+	
 
 	private Authentication tokenToAuth(String encodedToken) {
+		
+		
 		Token token;
+		Authentication auth;
 		try {
 			if(encodedToken != null) {
 				String decodedToken = cryptService.decrypt(encodedToken);
 				token = new ObjectMapper().readValue(decodedToken, Token.class);
+				auth = this.getAuthenticationManager().authenticate(
+						new UsernamePasswordAuthenticationToken(token.getEmail(), token.getPassword()));
 			} else {
 				token = new Token(null, null);
+				auth = null;
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			token = new Token(null, null);
+			auth = null;
 		}
-		return this.getAuthenticationManager().authenticate(
-				new UsernamePasswordAuthenticationToken(token.getEmail(), token.getPassword()));
+		return auth;
 	}
 
 	public void setCryptService(CryptService cryptService) {
